b607e5acb99fd0a6b7e68e14c5e019f68c4b6d8e
kitdallege <kit.dallege@gmail.com>
2017-09-21 00:26:42 -0500

    playing around with styles, cuz ugly stuff isn't fun.

--- src/Web.hs
+++ src/Web.hs
@@ -23,7 +23,7 @@ import qualified Web.Scotty.Trans                     as S
 import Network.HTTP.Types (status404)
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-import Text.Blaze.Html.Renderer.Pretty
+import Text.Blaze.Html.Renderer.Text
 import           Database.SQLite.Simple
 
 
@@ -36,6 +36,26 @@ newtype ConfigM a = ConfigM
   { runConfigM :: ReaderT Config IO a
   } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Config)
 
+stdHead :: H.Html
+stdHead = H.head $ do
+  H.title "PaSEO (pah-sey-oh)"
+  H.meta H.! A.name "viewport" H.! A.content "width=device-width, initial-scale=1"
+  -- H.link  H.! A.rel "stylesheet"
+  --         H.! A.type_ "text/css"
+  --         H.! A.href "//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"
+  -- H.link  H.! A.rel "stylesheet"
+  --         H.! A.type_ "text/css"
+  --         H.! A.href "//cdn.rawgit.com/necolas/normalize.css/master/normalize.css"
+  -- H.link  H.! A.rel "stylesheet"
+  --         H.! A.type_ "text/css"
+  --         H.! A.href "//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css"
+  H.link  H.! A.rel "stylesheet"
+          H.! A.type_ "text/css"
+          H.! A.href "//cdnjs.cloudflare.com/ajax/libs/pure/1.0.0/pure-min.css"
+  -- H.link  H.! A.rel "stylesheet"
+  --         H.! A.type_ "text/css"
+  --         H.! A.href "https://cdnjs.cloudflare.com/ajax/libs/mini.css/2.3.4/mini-dark.min.css"
+
 application :: S.ScottyT TL.Text ConfigM ()
 application = do
   S.middleware logStdoutDev
@@ -43,10 +63,10 @@ application = do
     let toLink e = H.a H.! A.href (H.toValue ("/" <> e)) $ H.toHtml e
     dir <- lift $ asks configStorageLocation
     files <- liftIO $ listDirectory (T.unpack dir)
-    S.html . TL.pack . renderHtml $ do
-      H.head $ H.title "PaSEO (pah-sey-oh)"
+    S.html . renderHtml $ H.docTypeHtml $ do
+      stdHead
       H.body $ do
-        H.h1 "PaSEO (pah-sey-oh)"
+        H.h1 $ H.a H.! A.href "/" $ "PaSEO (pah-sey-oh)"
         H.h3 "Previous Strolls"
         H.ul $ forM_ files (H.li . toLink)
   S.get "/:metrixDb" $ do
@@ -65,13 +85,15 @@ application = do
           [] -> -1
           (_:_:_) -> -1
       -- NOTE: join is way faster than subselect.
-      pageList <- liftIO $ withConnection (dbFile::String) $ \conn -> 
-        query_ conn "select pages.id, pages.page, count(page_links.id)  as num from pages join page_links on pages.id = page_links.link group by pages.id, pages.page order by num desc limit 20;" :: IO [(Int, Text, Int)]
-      S.html . TL.pack . renderHtml $ do
-        H.head $ H.title "PaSEO (pah-sey-oh)"
+      pageList <- liftIO $ withConnection (dbFile::String) $ \conn ->
+        query_ conn "select pages.id, pages.page, count(page_links.id)  as num from pages join page_links on pages.id = page_links.link group by pages.id, pages.page order by num desc limit 50;" :: IO [(Int, Text, Int)]
+      S.html . renderHtml $ H.docTypeHtml $ do
+        stdHead
         H.body $ do
-          H.h1 "PaSEO (pah-sey-oh)"
-          H.h3 ("Viewing: " >> H.toHtml mdb)
+          H.h1 $ H.a H.! A.href "/" $ "PaSEO (pah-sey-oh)"
+          H.h3 $ do
+            "Viewing Database: "
+            H.a H.! A.href (H.toValue ("/" <> mdb)) $ H.toHtml mdb
           H.dl $ do
             H.dt "Number of Pages: "
             H.dd (H.toHtml cntOfPages)
@@ -96,14 +118,16 @@ application = do
           (_:_:_) -> "unknown"
       metaTags <- liftIO $ withConnection (dbFile::String) $ \conn ->
         query conn "select repr from page_meta where page_meta.page = (?);" (Only pid) :: IO [Only Text]
-      S.html . TL.pack . renderHtml $ do
-        H.head $ H.title "PaSEO (pah-sey-oh)"
-        H.body $ do
-          H.h1 "PaSEO (pah-sey-oh)"
-          H.h3 ("Viewing Database: " >> H.toHtml mdb)
-          H.h3 ("Viewing Page: " >> H.toHtml pageHref)
-          H.h3 "Page MetaTags"
-          H.ul $ forM_ metaTags (H.li . H.toHtml . fromOnly)
+      S.html . renderHtml $ H.docTypeHtml $ do
+          stdHead
+          H.body $ do
+            H.h1 $ H.a H.! A.href "/" $ "PaSEO (pah-sey-oh)"
+            H.h3 $ do
+              "Viewing Database: "
+              H.a H.! A.href (H.toValue ("/" <> mdb)) $ H.toHtml mdb
+            H.h3 ("Viewing Page: " >> H.toHtml pageHref)
+            H.h3 "Page MetaTags"
+            H.ul $ forM_ metaTags (H.li . H.toHtml . fromOnly)
       else do
         S.status status404
         S.text $ "Error: " <> TL.pack dbFile
