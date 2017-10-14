cc90d0f35e019c441b30bcafa7e57cc2987d1738
kitdallege <kit.dallege@gmail.com>
2017-10-13 22:07:57 -0500

    Splitting up web models from site-scan models. going to pull in spider code, convert it to use persistent, and then integrate it into the view so it can be ran from a page.

--- src/Model.hs
+++ src/Model.hs
@@ -11,10 +11,15 @@ module Model where
 
 import ClassyPrelude.Yesod
 import Database.Persist.Quasi
+import Data.Time.LocalTime (TimeOfDay(..))
+import Data.Time.Calendar (Day(..))
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
-    $(persistFileWith lowerCaseSettings "config/models")
+    $(persistFileWith lowerCaseSettings "config/models/core")
+
+share [mkPersist sqlSettings, mkMigrate "migrateAllScan"]
+    $(persistFileWith lowerCaseSettings "config/models/scan")
