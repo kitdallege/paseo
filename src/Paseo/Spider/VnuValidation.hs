0a568e3252991c6b2e3ca8a38be7b2a4796b0cf4
kitdallege <kit.dallege@gmail.com>
2017-10-24 20:24:29 -0500

    cleanup after making it work.

--- src/Paseo/Spider/VnuValidation.hs
+++ src/Paseo/Spider/VnuValidation.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Paseo.Spider.VnuValidation
   (
@@ -7,33 +8,23 @@ module Paseo.Spider.VnuValidation
   , Message(..)
   , MessageType
   , Messages
-  , main
+  , test
   ) where
-import           Data.Aeson
-import           Data.Aeson.Encode.Pretty
-import           Data.Aeson.Types
-import Data.Word8 (isSpace)
-import qualified Data.ByteString            as BS
-import qualified Data.ByteString.Char8      as BS8
-import qualified Data.ByteString.Lazy       as BSL
-import qualified Data.Char                  as Char
-import           Data.Semigroup             ((<>))
-import qualified Data.Text                  as T
-import qualified Data.Text.Encoding         as TE
-import qualified Data.Text.IO               as TIO
-import           Data.Text                  (Text)
-import qualified Data.Vector                as V
-import           GHC.Generics
-import           Prelude                    hiding (log)
-import           System.Exit
-import           System.IO
-import           System.Process             hiding (readCreateProcessWithExitCode)
-import           System.Process.ByteString.Lazy
-
-vnuJarFile :: String
-vnuJarFile = "/home/kit/vnu.jar"
--- all output is in stderr [not sure why]
--- java -jar ~/vnu.jar --format json /tmp/lvs-home.html 2>&1
+import           Data.Aeson                     (eitherDecode)
+import           Data.Aeson.Encode.Pretty       (Indent (..), confIndent,
+                                                 defConfig, encodePretty')
+import           Data.Aeson.Types               (Options (..), defaultOptions,
+                                                 fieldLabelModifier,
+                                                 genericParseJSON,
+                                                 genericToJSON,
+                                                 omitNothingFields)
+import qualified Data.ByteString.Lazy           as BSL
+import qualified Data.Char                      as Char
+import qualified Data.Vector                    as V
+import           Import
+import           System.Exit                    (ExitCode (..))
+import           System.Process                 (proc)
+import           System.Process.ByteString.Lazy (readCreateProcessWithExitCode)
 
 data MessageType =
       MsgError
@@ -49,8 +40,10 @@ data MessageType =
 messageTypeOptions :: Options
 messageTypeOptions = defaultOptions {
               constructorTagModifier = map Char.toLower . drop 3}
+
 instance ToJSON MessageType where
     toJSON = genericToJSON messageTypeOptions
+
 instance FromJSON MessageType where
     parseJSON = genericParseJSON messageTypeOptions
 
@@ -75,6 +68,7 @@ messageOptions = defaultOptions {fieldLabelModifier = lowerOne . drop 3}
 
 instance ToJSON Message where
     toJSON = genericToJSON messageOptions
+
 instance FromJSON Message where
     parseJSON = genericParseJSON messageOptions
 
@@ -87,35 +81,34 @@ data ValidationResults = ValidationResults
     } deriving (Show, Eq, Ord, Generic)
 
 validationResultsOptions :: Options
-validationResultsOptions = defaultOptions {
-              fieldLabelModifier = map Char.toLower . drop 10
+validationResultsOptions = defaultOptions
+            { fieldLabelModifier = map Char.toLower . drop 10
             , omitNothingFields = True
-          }
+            }
+
 instance ToJSON ValidationResults where
     toJSON = genericToJSON validationResultsOptions
 
 instance FromJSON ValidationResults where
     parseJSON = genericParseJSON validationResultsOptions
 
-gatherOutput :: ProcessHandle -> Handle -> IO (ExitCode, BSL.ByteString)
-gatherOutput ph h = work mempty
-  where
-    work acc = do
-        -- Read any outstanding input.
-        bs <- BSL.hGetNonBlocking h (64 * 1024)
-        let acc' = acc <> bs
-        -- Check on the process.
-        s <- getProcessExitCode ph
-        -- Exit or loop.
-        case s of
-            Nothing -> work acc'
-            Just ec -> do
-                -- Get any last bit written between the read and the status
-                -- check.
-                last' <- BSL.hGetContents h
-                return (ec, acc' <> last')
 
-vnuValidateBSL :: BSL.ByteString -> IO (Either String ValidationResults)
+-- TODO: This needs to be a parameter (or) programmatic.
+-- all output is in stderr [not sure why]
+-- java -jar ~/vnu.jar --format json /tmp/lvs-home.html 2>&1
+vnuJarFile :: String
+vnuJarFile = "/home/kit/vnu.jar"
+
+-- | Validate a HTML document with the vnu.jar from "The Nu Html Checker (v.Nu)"
+-- which is the backend to
+--  * checker.html5.org
+--  * html5.validator.nu
+--  * validator.w3.org/nu
+-- see: https://validator.github.io/validator/ for more information
+-- The 'ValidationResults' are returned which contain all the fields
+-- from the jar's output.
+vnuValidateBSL :: LByteString                   -- ^ HTML page to validate
+    -> IO (Either String ValidationResults)     -- ^ Results of validation or a String error if the subprocess (or) json decode fail.
 vnuValidateBSL f = do
     let ps = proc "java" ["-jar", vnuJarFile, "--exit-zero-always", "--format", "json", "-"]
     (exitCode, _, err) <- readCreateProcessWithExitCode ps f
@@ -123,8 +116,8 @@ vnuValidateBSL f = do
         ExitSuccess -> return $ eitherDecode err
         ExitFailure e -> return . Left $ "ExitCodeFailure: " <> show exitCode <> " Error: " <> show e
 
-main :: IO ()
-main = do
+test :: IO ()
+test = do
   f <- BSL.readFile "/tmp/lvs-home.html"
   results <- vnuValidateBSL f
   case results of
